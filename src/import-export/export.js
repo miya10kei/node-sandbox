3a3dc35af6dda036313d4fa15568e45a2772bc12
Keisuke Miyaushiro <miya10kei@gmail.com>
2019-02-17 14:36:57 +0000

    verify import/export

---
+++ src/import-export/export.js
@@ -0,0 +1,11 @@
+export default () => {
+  console.log('export default')
+}
+
+export function module1() {
+  console.log('export module1')
+}
+
+export function module2() {
+  console.log('export module2')
+}
---
+++ src/import-export/import.js
@@ -0,0 +1,5 @@
+import Module , { module1, module2 } from './export'
+
+Module()
+module1()
+module2()
